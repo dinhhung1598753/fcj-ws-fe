927772d4dc2584475752c35cf229216c22821575
Hung.Hoang <Hung.Hoang@aircanada.ca>
2024-09-04 20:03:24 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 import {
-  createHashRouter,
+  createBrowserRouter,
   Navigate,
   RouterProvider,
 } from "react-router-dom";
@@ -29,7 +29,7 @@ const ProtectedRouteLogin = ({ isAuth }) => {
   return <Login />;
 };
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     index: true,
